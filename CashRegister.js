cc59000a7abd56b5dd5adb5c1c3320e9a975a7c7
Cameron Chardukian <camchardukian@gmail.com>
2019-04-17 22:13:12 +0700

    Second draft of Cash Register project

--- CashRegister.js
+++ CashRegister.js
@@ -1,34 +1,34 @@
-// I've built a basic currency table thus far, and created a change variable with
-// a formula to calculate change accurately. I've still got a LONG way to go, however.
-// This problem is pretty tough and clearly shows how important breaking things
-// down into small achievable blocks is. 
+// I realized that my previous idea of testing whether cid was equal to change
+// didn't make much sense. This is because the change would be a single number/integer,
+// whereas the cid argument would always be an array. Thus, it would basically be impossible
+// for the two to ever equal each other. Rather, what I need to do is use the reduce method
+// on cid to calculate the sum of all the values in the drawer.
 
-
-  let currencyUnits = [
-    {name: 'ONE HUNDRED', value: 100.00},
-    {name: 'TWENTY', value: 20.00},
-    {name: 'TEN', value: 10.00},
-    {name: 'FIVE', value: 5.00},
-    {name: 'ONE', value: 1.00},
-    {name: 'QUARTER', value: 0.25},
-    {name: 'DIME', value: 0.10},
-    {name: 'NICKEL', value: 0.05},
-    {name: 'PENNY', value: 0.01}
-  ];
-
-function checkCashRegister(price, cash, cid) {
-  let statusObj = {status: null, change: []};
-  let change = cash - price;
-  if (cid == change) {
-    statusObj.status = "CLOSED"
-    statusObj.change = change;
-    return statusObj;
-  }
-else if (cid < change) {
-  statusObj.status = "INSUFFICIENT_FUNDS";
-  return statusObj;
+ let currencyObj = [
+  {name : 'ONE HUNDRED', val: 100.00},
+  {name : 'TWENTY', val: 20.00},
+  {name : 'TEN', val: 10.00},
+  {name : 'FIVE', val: 5.00},
+  {name : 'ONE', val: 1.00},
+  {name : 'QUARTER', val: 0.25},
+  {name : 'DIME', val: 0.10},
+  {name : 'NICKEL', val: 0.05},
+  {name: 'PENNY', val: 0.01}
+];
+function checkCashRegister (price, cash, cid) {
+let statusObj = {status: null, change: []};
+let change = cash - price;
+let register = cid.reduce(function(acc, current){
+  acc.total += current[1];
+  acc[current[0]] = current[1];
+  return acc;
+}, {total: 0});
 }
-}
+
+// We can view the total amount of cash in the drawer by accessing register.total.
+// I've already spent nearly four hours thinking about this problem today and doing research
+// so I'm going to call it a night now.
+// I'll add more notes, and try to make more progress on it tomorrow.
 
 
 checkCashRegister(1900, 20000, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
